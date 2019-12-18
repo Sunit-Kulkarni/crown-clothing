cf2411df1cabd7666c9ac27581e0596a9c7815d1
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2019-12-17 21:43:18 -0500

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ class App extends Component {
             ...snapshot.data()
           }
         })
-        console.log(this.state);
       })
       } else {
         this.setState({ currentUser: userAuth })
